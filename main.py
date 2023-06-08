9050f2f0d560de225e6515a6e5c4f61dba00c3d2
Sanchitha Udana Wijesundara <109952575+SanchithaUdana@users.noreply.github.com>
2023-06-08 20:08:34 +0530

    first commit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # findArea is the package name
 from findArea import areaOfShapes as shape
 from findArea import areaOfObjects as object
+from findArea import areaOfCombinedShapes as comshape
+from findArea import areaOfCombinedObject as comobj
 
 shape.circle(7)
 shape.square(12, 45)
 shape.trapezium(34, 5)
-
-
