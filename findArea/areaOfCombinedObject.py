ce6d06f432e43b81f56e5f9ef7d57f21deec17a6
Sanchitha Udana Wijesundara <109952575+SanchithaUdana@users.noreply.github.com>
2023-06-10 21:30:38 +0530

    first commit

--- findArea/areaOfCombinedObject.py
+++ findArea/areaOfCombinedObject.py
@@ -1,2 +1,16 @@
 from findArea import areaOfObjects as object
+from findArea import areaOfShapes as shape
+
+
+def cubeHemisphere(length, radius):
+    area = (object.cube(length) - shape.square(length)) + (object.hemisphere(radius) - shape.circle(radius))
+    return area
+
+
+def cubeSphere(length, radius):
+    area = object.cube(radius) + object.cube(length)
+    return area
+
+
+
 
