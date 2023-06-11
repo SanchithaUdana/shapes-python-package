82b69e3d1456db0902abf0e876733f4bbdb3537d
Sanchitha Udana Wijesundara <109952575+SanchithaUdana@users.noreply.github.com>
2023-06-11 09:52:43 +0530

    findArea

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from findArea import areaOfObjects as object
 from findArea import areaOfCombinedShapes as comShape
 from findArea import areaOfCombinedObject as comObj
 
-print(round((shape.circle(7)), 2))
-print(round((object.Cylinder(7, 12)), 2))
-print(round((comShape.squareTriangle(4, 5, 3)), 2))
-print(round((comObj.cylinderCone(7, 8, 4)), 2))
+print("Area of circle : ", round((shape.circle(7)), 2), "squire unit")
+print("Area of cylinder :  ", round((object.Cylinder(7, 12)), 2), "squire unit")
+print("Area of Square Triangle :  ", round((comShape.squareTriangle(4, 5, 3)), 2), "squire unit")
+print("Area of Cylinder Cone : ", round((comObj.cylinderCone(7, 8, 4)), 2), "squire unit")
