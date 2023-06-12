bd707a1d62e06224ecdf663e9350c590197a3c4b
Sanchitha Udana Wijesundara <109952575+SanchithaUdana@users.noreply.github.com>
2023-06-12 16:25:04 +0530

    findArea

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ print("Area of circle : ", round((shape.circle(7)), 2), "squire unit")
 print("Area of cylinder :  ", round((object.Cylinder(7, 12)), 2), "squire unit")
 print("Area of Square Triangle :  ", round((comShape.squareTriangle(4, 5, 3)), 2), "squire unit")
 print("Area of Cylinder Cone : ", round((comObj.cylinderCone(7, 8, 4)), 2), "squire unit")
+
+# Example Function Calling using findArea package
+shape.square(2)
+object.cone(7, 12)
+comShape.hemiCircleSquire(7, 12)
+comObj.cubeHemisphere(12, 7)
+# we can call all of functions using own object name
+
+
+
